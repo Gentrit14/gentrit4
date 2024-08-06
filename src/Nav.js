20bf90cf2e8dcfcfd482ca87eee9bdf307017deb
gentrits1 <gentritsinani75@gmail.com>
2024-08-06 02:11:00 +0200

    GENT

--- src/Nav.js
+++ src/Nav.js
@@ -2,17 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.scss'
 
+import { AiFillTwitch } from "react-icons/ai";
+import { GoChevronDown } from "react-icons/go";
+import ProductsMegamenu from './ProductsMegamenu';
+
 const Nav = () => {
   return (
     <div className='nav'>
-        <Link to='/' className='title'>Gentirt</Link>
+        <Link to='/' className='title'><AiFillTwitch />Twitch</Link>
 
         <div className='links'>
-           <Link to='/Blog'>Blog</Link>
-           <Link to='/Product'>Product</Link>
-           <Link to='/About'>About Us</Link>
+           <div className='nav-link'>
+              <Link to='/' className='inner-link'>Products <GoChevronDown /></Link>  
+              <ProductsMegamenu />  
+           </div>
+           <div className='nav-link'>
+              <Link to='/' className='inner-link'>Blog </Link>    
+           </div>
+           <div className='nav-link'>
+              <Link to='/' className='inner-link'>About </Link>    
+           </div>
+           <div className='nav-link'>
+              <Link to='/' className='inner-link'>Contact </Link>    
+           </div>
         </div>
 
+        <div className='auth'>
+            <Link to='/' className='login'>Log In</Link>
+            <Link to='/' className='signup'>Sign Up</Link>
+        </div>
     </div>
   )
 }
